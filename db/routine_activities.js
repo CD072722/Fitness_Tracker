a6e69caf8c5aa0b8024cb89bb89f91c4ddc30e70
jwwalls <jwwalls@users.noreply.github.com>
2023-05-04 20:42:14 -0500

    routines API done

--- db/routine_activities.js
+++ db/routine_activities.js
@@ -14,7 +14,7 @@ async function addActivityToRoutine({
     } = await client.query(
       `
   INSERT INTO routine_activities("routineId", "activityId", count, duration) 
-  VALUES($1, $2, $3, $4) 
+  VALUES($1, $2, $3, $4)   
   RETURNING *;
 `,
       [routineId, activityId, count, duration]
