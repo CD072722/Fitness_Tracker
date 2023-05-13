cf9330cad97a72d06b6a3f03ecbe7c5b22e22540
jwwalls <121780225+jwwalls@users.noreply.github.com>
2023-05-12 22:20:59 -0500

    update

--- db/client.js
+++ db/client.js
@@ -1,10 +1,13 @@
-const { Pool } = require('pg');
+const { Pool } = require("pg");
 
-const connectionString = process.env.DATABASE_URL || 'https://localhost:5432/fitness-dev';
+const connectionString =
+  "postgres://doexyhoxcxszep:3dd3c2ae1afe51d52864327eb46742562edf4d1949d45cfc27a030f8e37127b3@ec2-3-208-74-199.compute-1.amazonaws.com:5432/dmc53rufqfdoi";
 
 const client = new Pool({
   connectionString,
-  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : undefined,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 module.exports = client;
