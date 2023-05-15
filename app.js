259473e5c16bca3476539ff17edc1d41563b3be3
jwwalls <jwwalls@users.noreply.github.com>
2023-05-14 22:43:55 -0500

    deployed

--- app.js
+++ app.js
@@ -1,47 +1,40 @@
-require("dotenv").config();
-const express = require("express");
-const app = express();
-const client = require("./db/client");
-const cors = require("cors");
-const path = require("path");
+require("dotenv").config()
+const express = require("express")
+const app = express()
+const client= require ('./db/client')
+const cors =require('cors')
 
 client.connect();
 
-app.use(cors());
-const morgan = require("morgan");
-app.use(morgan("dev"));
+app.use(cors())
+const morgan = require('morgan');
+app.use(morgan('dev'));
 
-app.use(express.json());
+app.use(express.json())
 
-const apiRouter = require("./api");
-app.use(express.static(path.join(__dirname, "./client/dist")));
-app.use("/api", apiRouter);
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./client/dist", "index.html"));
+const apiRouter = require('./api');
+app.use('/api', apiRouter);
+app.get('*', (req, res) => {
+  res.status(404).send({error: '404 - Not Found', message: 'No route found for the requested URL'});
 });
+
 // error handling middleware
 app.use((error, req, res, next) => {
-  if (res.statusCode < 400) res.status(500);
-  res.send({
-    error: error.message,
-    name: error.name,
-    message: error.message,
-    table: error.table,
-  });
-});
-
-app.use((error, req, res, next) => {
-  res.sendFile(path.join(__dirname, "./client/dist", "index.html"));
+ 
+  if(res.statusCode < 400) res.status(500);
+  res.send({error: error.message, name: error.name, message: error.message, table: error.table});
 });
 
 app.use((req, res, next) => {
-  console.log("<____Body Logger START____>");
-  console.log(req.body);
-  console.log("<_____Body Logger END_____>");
+    console.log("<____Body Logger START____>");
+    console.log(req.body);
+    console.log("<_____Body Logger END_____>");
+    
+    next();
+  });
 
-  next();
-});
+  
 
 // Setup your Middleware and API Router here
 
